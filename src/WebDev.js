88ff4ac2736f4040f0f071aca1df5b1d6bffec2a
Kana <st.dio.kail@gmail.com>
2022-04-26 19:46:57 +0100

    added new project

--- src/WebDev.js
+++ src/WebDev.js
@@ -17,6 +17,32 @@ export default function WedDev () {
             </div>
         </div>
 
+
+        <div class = "grid grid-cols-1" >
+            <div class = "web-portfolio-description m-auto">
+                <div class = "grid grid-cols-5">
+                <div class = "col-span-3"><p class = "web-portfolio-card-title" style = {{border:0}}> WEBSITE TEMPLATE</p> <br></br> </div>
+                <div class = "ylink-container">  <img src={figma} alt={"figma"} class = {"web-link"} onClick={(e) => {e.preventDefault(); window.open('https://www.figma.com/file/AB0pULHAnsgWMeSAy6qAMl/RESEARCH-WEBSITE-TEMPLATE-%3A-STUDIO-KAIL?node-id=0%3A1');}}/> </div>
+                <div class = "ylink-container">  <img src={weblink} alt={"weblink"} class = {"web-link"} onClick={(e) => {e.preventDefault(); window.open('https://stdiokail.github.io/reseach-website-template/#/');}}/> </div>
+                </div>
+                <div class = "grid grid-cols-1">
+                <p class = "web-portfolio-body"> 
+                This website template includes a landing page, 2 content pages, and a contact page. The deployed website is responsive and includes a slide-in menu viewable on small/medium screens. The following files are included in the showcase: <br/> <br/>
+               <p class = "text-indented" > &#8226; Figma file for both the wireframe and coloured version <br/> <br/>
+                &#8226; Interactive prototyping (available on Figma)  <br/> <br/>
+                &#8226; Deployed website hosted on the studio's GitHub, showcasing the functionality of the website including responsiveness  <br/> <br/>
+                &#8226; Source code (available on GitHub) <br/> <br/> </p>
+                Website made using React and Tailwind and utilised predominantly Javascript, HTML and CSS. Logo and icons were made using Adobe Illustrator.        </p>
+                </div>
+            </div>
+            </div>
+
+            <div class = "grid grid-cols-1 md:grid-cols-2 all-image">
+                <img src={mobileui} alt={"mobile-ver"} style = {{padding: 50}}/>
+                <img src={webui} alt={"web-ver"} style = {{padding: 50}}/>
+         </div>
+
+
         <div class = "grid grid-cols-1" >
             <div class = "web-portfolio-description m-auto">
                 <div class = "grid grid-cols-5">
@@ -42,29 +68,6 @@ export default function WedDev () {
          </div>
 
 
-        <div class = "grid grid-cols-1" >
-            <div class = "web-portfolio-description m-auto">
-                <div class = "grid grid-cols-5">
-                <div class = "col-span-3"><p class = "web-portfolio-card-title" style = {{border:0}}> WEBSITE TEMPLATE</p> <br></br> </div>
-                <div class = "ylink-container">  <img src={figma} alt={"figma"} class = {"web-link"} onClick={(e) => {e.preventDefault(); window.open('https://www.figma.com/file/imNybNYHYMgh5Jgb9Fr79i/Scientific-Research-Firm-Landing-Page-Template---STUDIO-KA-IL?node-id=0%3A1');}}/> </div>
-                <div class = "ylink-container">  <img src={weblink} alt={"weblink"} class = {"web-link"} onClick={(e) => {e.preventDefault(); window.open('https://stdiokail.github.io/reseach-website-template/#/');}}/> </div>
-                </div>
-                <div class = "grid grid-cols-1">
-                <p class = "web-portfolio-body"> 
-                This website template includes a landing page, 2 content pages, and a contact page. The deployed website is responsive and includes a slide-in menu viewable on small/medium screens. The following files are included in the showcase: <br/> <br/>
-               <p class = "text-indented" > &#8226; Figma file for both the wireframe and coloured version <br/> <br/>
-                &#8226; Interactive prototyping (available on Figma)  <br/> <br/>
-                &#8226; Deployed website hosted on the studio's GitHub, showcasing the functionality of the website including responsiveness  <br/> <br/>
-                &#8226; Source code (available on GitHub) <br/> <br/> </p>
-                Website made using React and Tailwind and utilised predominantly Javascript, HTML and CSS. Logo and social icons were made using Adobe Illustrator. Illustrations made by Vikasuh.                 </p>
-                </div>
-            </div>
-            </div>
-
-            <div class = "grid grid-cols-1 md:grid-cols-2 all-image">
-                <img src={mobileui} alt={"mobile-ver"} style = {{padding: 50}}/>
-                <img src={webui} alt={"web-ver"} style = {{padding: 50}}/>
-         </div>
 
             <div class = "navigation-portfolios grid grid-cols-3" style = {{marginTop:20}}>
            <div> <Link to ="./"> <button class = "pnav-button" style = {{marginRight: 30}}> HOME </button> </Link>  </div>
